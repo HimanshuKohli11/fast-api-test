241a03958f384819fee3657f6410f9c2c338479c
Himanshu Kohli <39043823+HimanshuKohli11@users.noreply.github.com>
2022-11-02 05:22:08 -0700

    Update db_conn.py

--- db_conn.py
+++ db_conn.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-DATABASE_URL = "postgresql://postgres:himanshu@localhost:5432/test_user"
+DATABASE_URL = "postgres://thqzghojnxgfvi:e7db00a08fd236f0e15d1ccfefb41d9891b3d0c03cc6ba9a46db3786f24884af@ec2-52-23-131-232.compute-1.amazonaws.com:5432/d2vbttev840ial
+"
 
 engine = create_engine(DATABASE_URL, echo=True)
 Base = declarative_base()
