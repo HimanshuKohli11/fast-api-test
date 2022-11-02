68a7e2bda45b43afcf11751c04b745295de79b4f
Himanshu Kohli <39043823+HimanshuKohli11@users.noreply.github.com>
2022-11-02 09:39:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ from pydantic import BaseModel
 import models
 from db_conn import SessionLocal
 
+import os
+ON_HEROKU = os.environ.get('ON_HEROKU')
+
+if ON_HEROKU:
+    # get the heroku port
+    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
+else:
+    port = 8000
+
 
 app = FastAPI()
 
