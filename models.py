8f4c65a5052b4aeebef852edc224465848b3b2f8
Himanshu Kohli <39043823+HimanshuKohli11@users.noreply.github.com>
2022-12-01 21:12:42 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,12 @@
-from db_conn import Base
-from typing import Optional, List
-from uuid import UUID, uuid4
-from sqlalchemy import String, Integer, Column
+# from db_conn import Base
+# from typing import Optional, List
+# from uuid import UUID, uuid4
+# from sqlalchemy import String, Integer, Column
 
 
-class User(Base):
-    __tablename__ = 'test_user_tbl'
+# class User(Base):
+#     __tablename__ = 'test_user_tbl'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    description = Column(String, nullable=False)
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String, nullable=False)
+#     description = Column(String, nullable=False)
