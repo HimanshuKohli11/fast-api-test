cf7825180f9bacde518e68187a4f1953c00c4330
Himanshu Kohli <39043823+HimanshuKohli11@users.noreply.github.com>
2022-12-01 21:10:59 +0530

    Update db_conn.py

--- db_conn.py
+++ db_conn.py
@@ -1,22 +1,22 @@
-import urllib.parse
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+# import urllib.parse
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
 
 
-DATABASE_URL="postgres://thqzghojnxgfvi:e7db00a08fd236f0e15d1ccfefb41d9891b3d0c03cc6ba9a46db3786f24884af@ec2-52-23-131-232.compute-1.amazonaws.com:5432/d2vbttev840ial
-"
+# DATABASE_URL="postgres://thqzghojnxgfvi:e7db00a08fd236f0e15d1ccfefb41d9891b3d0c03cc6ba9a46db3786f24884af@ec2-52-23-131-232.compute-1.amazonaws.com:5432/d2vbttev840ial
+# "
 
-engine = create_engine(DATABASE_URL, echo=True)
-Base = declarative_base()
-SessionLocal = sessionmaker(bind=engine)
+# engine = create_engine(DATABASE_URL, echo=True)
+# Base = declarative_base()
+# SessionLocal = sessionmaker(bind=engine)
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
 
-    except Exception as e:
-        print("Exception Occurred!! {0}".format(e))
-        db.close()
+#     except Exception as e:
+#         print("Exception Occurred!! {0}".format(e))
+#         db.close()
