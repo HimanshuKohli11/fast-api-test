d03787032a7778c8f75977dfc81e290bcd8c14e5
Himanshu Kohli <39043823+HimanshuKohli11@users.noreply.github.com>
2022-12-01 21:07:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,30 +3,30 @@ from uuid import uuid4, UUID
 from typing import Optional
 from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
-
 import models
-from db_conn import SessionLocal
-
+# from db_conn import SessionLocal
 import os
-ON_HEROKU = os.environ.get('ON_HEROKU')
 
-if ON_HEROKU:
-    # get the heroku port
-    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
-else:
-    port = 8000
+
+# ON_HEROKU = os.environ.get('ON_HEROKU')
+
+# if ON_HEROKU:
+#     # get the heroku port
+#     port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
+# else:
+#     port = 8000
 
 
 app = FastAPI()
 
 
-class User(BaseModel):
-    id: Optional[int]
-    name: str
-    description: str
+# class User(BaseModel):
+#     id: Optional[int]
+#     name: str
+#     description: str
 
-    class Config:
-        orm_mode = True
+#     class Config:
+#         orm_mode = True
 
 
 # db: List[User] = [
@@ -41,56 +41,61 @@ class User(BaseModel):
 #         description="Q",
 #     )
 # ]
-db = SessionLocal()
+# db = SessionLocal()
 
 
-@app.get("/getting/", response_model=List[User], status_code=200)
-def get_user():
-    # return db
-    user_list = db.query(models.User).all()
-    return user_list
+# @app.get("/getting/", response_model=List[User], status_code=200)
+# def get_user():
+#     # return db
+#     user_list = db.query(models.User).all()
+#     return user_list
 
 
-@app.post("/creating/", response_model=User, status_code=status.HTTP_201_CREATED)
-def post_user(user: User):
-    db_user = db.query(models.User).filter(models.User.name == user.name).first()
-    if db_user is not None:
-        raise HTTPException(status_code=400, detail="User already exists!")
+# @app.post("/creating/", response_model=User, status_code=status.HTTP_201_CREATED)
+# def post_user(user: User):
+#     db_user = db.query(models.User).filter(models.User.name == user.name).first()
+#     if db_user is not None:
+#         raise HTTPException(status_code=400, detail="User already exists!")
 
-    new_user = models.User(
-        name=user.name,
-        description=user.description
-    )
+#     new_user = models.User(
+#         name=user.name,
+#         description=user.description
+#     )
 
-    db.add(new_user)
-    db.commit()
+#     db.add(new_user)
+#     db.commit()
 
-    return new_user
-
-
-@app.put("/updating/{uid}", response_model=User, status_code=status.HTTP_200_OK)
-def put_user(uid: int, user: User):
-    # db.append(user)
-    new_user = db.query(models.User).filter(models.User.id==uid).first()
-    new_user.name = user.name
-    new_user.description = user.description
-
-    db.commit()
-
-    return new_user
-    # return {'name': user.name}
+#     return new_user
 
 
-@app.delete("/deleting/{uid}")
-def deleting_user(uid: int):
-    db_user = db.query(models.User).filter(models.User.id == uid).first()
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User doesn't exists!")
+# @app.put("/updating/{uid}", response_model=User, status_code=status.HTTP_200_OK)
+# def put_user(uid: int, user: User):
+#     # db.append(user)
+#     new_user = db.query(models.User).filter(models.User.id==uid).first()
+#     new_user.name = user.name
+#     new_user.description = user.description
 
-    db.delete(db_user)
-    db.commit()
+#     db.commit()
 
-    return db_user
+#     return new_user
+#     # return {'name': user.name}
+
+
+# @app.delete("/deleting/{uid}")
+# def deleting_user(uid: int):
+#     db_user = db.query(models.User).filter(models.User.id == uid).first()
+#     if db_user is None:
+#         raise HTTPException(status_code=404, detail="User doesn't exists!")
+
+#     db.delete(db_user)
+#     db.commit()
+
+#     return db_user
 
 
 # app.run(host='0.0.0.0', port=port)
+
+
+@app.get("/", status_code=200)
+def get_user():
+    return {'message': 'Success'}
