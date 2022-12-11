e4ba253d492f8c1c819fbeab30cb40f0d16063a5
Himanshu Kohli <39043823+HimanshuKohli11@users.noreply.github.com>
2022-12-11 14:24:20 +0530

    Update db_conn.py

--- blog/db_conn.py
+++ blog/db_conn.py
@@ -3,9 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///test.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///test.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+SQLALCHEMY_DATABASE_URL = 'postgres://postgresql_pgadmin_moat_user:CCfVVcSRpMgusmMXUUgmG9s8JSHoffve@dpg-ceapfoha6gdichj3d6q0-a/postgresql_pgadmin_moat'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
